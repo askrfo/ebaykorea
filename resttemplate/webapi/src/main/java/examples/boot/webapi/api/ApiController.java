cefdb8dd211d55c64ba9d7a10506dbd5850c70d9
김성박 <urstory@gmail.com>
2018-06-28 18:45:57 +0900

    profile 설정 추가

--- resttemplate/webapi/src/main/java/examples/boot/webapi/api/ApiController.java
+++ resttemplate/webapi/src/main/java/examples/boot/webapi/api/ApiController.java
@@ -1,6 +1,7 @@
 package examples.boot.webapi.api;
 
 import examples.boot.webapi.dto.Book;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +17,10 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/books")
 public class ApiController {
+    // my.address 설정값을 주입하라.
+    @Value("${my.address}")
+    private String address;
+
 //    @GetMapping(path ="/{id}")
 //    public Book getBooks(@PathVariable(name = "id") Long id){
 //        Book book = new Book(id, "hello");
@@ -30,6 +35,7 @@ public class ApiController {
 
     @GetMapping
     public ResponseEntity<List<Book>> getBooks(){
+        System.out.println("address: " + address);
         List<Book> list = new ArrayList<>();
         Book book1 = new Book(1L, "hello");
         Book book2 = new Book(2L, "hi");
