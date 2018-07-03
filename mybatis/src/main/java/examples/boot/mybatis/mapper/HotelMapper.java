1cbec8304bf775897811d49b8639d69ffb3bff75
김성박 <urstory@gmail.com>
2018-07-03 16:05:42 +0900

    MyBatis 수정

--- mybatis/src/main/java/examples/boot/mybatis/mapper/HotelMapper.java
+++ mybatis/src/main/java/examples/boot/mybatis/mapper/HotelMapper.java
@@ -2,9 +2,11 @@ package examples.boot.mybatis.mapper;
 
 import examples.boot.mybatis.domain.Hotel;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface HotelMapper {
 
+    @Select("select * from hotel where city = #{city_id}")
     Hotel selectByCityId(long city_id);
 }
